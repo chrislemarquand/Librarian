3187c823b65c6aa7c294445a994ea17933ac501d
Chris Le Marquand <chris.lemarquand@gmail.com>
2026-03-17 14:27:59 +0000

    Initial Commit

---
+++ Librarian/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Librarian
+//
+//  Created by Chris Le Marquand on 17/03/2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Librarian/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Librarian
+//
+//  Created by Chris Le Marquand on 17/03/2026.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
